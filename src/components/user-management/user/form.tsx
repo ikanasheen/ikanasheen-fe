20ce9c08e15962de5b680c204a4b634cb8be311d
Yeni Nur Azizah <ikanasheen.co@gmail.com>
2023-03-08 14:46:49 +0700

    create menu user

--- src/components/user-management/user/form.tsx
+++ src/components/user-management/user/form.tsx
@@ -1,24 +1,14 @@
 import { useRef, useState } from "react";
 import { FormGroupModel, FormRef, BgsForm, BgsGroupForm, BgsButton } from "@andrydharmawan/bgs-component";
-import { isArray, mounted } from "lib";
-import FileHelper from "helper/FileHelper";
-import RoleHelper from "helper/RoleHelper";
-import UserHelper from "helper/UserHelper";
+import {  mounted } from "lib";
+import UserManagementHelper from "helper/user-management/UserManagementHelper";
 import DrawerLayout, { DrawerRenderProps } from "shared/layout/drawer-layout";
-import MoreHorizRoundedIcon from '@mui/icons-material/MoreHorizRounded';
-import SalesCategoryHelper from "helper/SalesCategoryHelper";
-import TerritoryHelper from "helper/TerritoryHelper";
-import { RolesEnum, ServiceNameUploadConst } from "consts";
-import Image from "components/file/components/image";
+import StatusConst from "consts/status.const";
 
 export default function UserManagementUserForm({ title, mode, id, hide, onSuccess = () => {} }: DrawerRenderProps) {
     const formRef = useRef<FormRef>(null);
     const [loading, setLoading] = useState<boolean>(false);
-    let showPassword: boolean = false;
-
-    const visibleSalesCoordinator = (roleCode: string) => {
-        formRef.current?.itemOption("salesCategory.id").option("visible", roleCode !== RolesEnum.SALES_COORDINATOR)
-    }
+    // const userId = credential.storage.get("user").idUser;
 
     const form: FormGroupModel = {
         apperance: "filled",
@@ -26,158 +16,36 @@ export default function UserManagementUserForm({ title, mode, id, hide, onSucces
         showLabelShrink: true,
         onSubmit: (values) => {
             setLoading(true);
-            values.roles === RolesEnum.SALES_COORDINATOR && delete values.salesCategory;
-
-            values.roles = [{
-                code: values.roles
-            }];
-
-            delete values.showPassword;
-
-
-            UserHelper.createupdate(values, values.idUser, ({ status }) => {
+            UserManagementHelper.createupdate(values, values.idUser, ({ status }) => {
                 setLoading(false);
                 if (status) onSuccess();
             })
-        },
-        formData: {
-            enabled: true
         },
         item: {
             main: {
                 spacing: 3,
                 items: [
                     {
-                        dataField: "fullname",
+                        dataField: "idUser",
                         editorOptions: {
-                            autoComplete: "off"
+                            disabled: true
                         },
                         label: {
-                            icon: "user",
-                            text: "Nama Lengkap"
+                            text: "ID User"
                         },
-                        validationRules: ["required"]
                     },
-                    `email|label.icon=mail|validationRules=required,email|editorOptions.disabled=${!!id}`,
-                    `phoneNumber|label.text=Nomor Telepon|label.icon=phone|validationRules=required,maxLength.15,pattern.number`,
                     {
-                        dataField: "roles",
+                        dataField: "status",
+                        label: {
+                            text: "Status"
+                        },
                         editorType: "select",
-                        label: {
-                            text: "Role",
-                            icon: "tree"
-                        },
                         editorOptions: {
-                            helper: (data) => RoleHelper.retrieve(data),
-                            displayExpr: "description",
-                            search: "description",
-                            valueExpr: "code",
-                            onChange: ({ value }) => visibleSalesCoordinator(value)
+                            dataSource: StatusConst,
+                            displayExpr: "display",
+                            valueExpr: "value",
                         },
-                        validationRules: ["required"]
                     },
-                    {
-                        dataField: "salesCategory.id",
-                        editorType: "select",
-                        label: {
-                            text: "Kategori Sales",
-                            icon: "tree"
-                        },
-                        editorOptions: {
-                            helper: data => SalesCategoryHelper.retrieve(data),
-                            displayExpr: "salesCategoryName",
-                            valueExpr: "id",
-                            search: "salesCategoryName"
-                        },
-                        validationRules: ["required"]
-                    },
-                    {
-                        dataField: "territory.id",
-                        editorType: "select",
-                        label: {
-                            text: "Territory",
-                            icon: "tree"
-                        },
-                        editorOptions: {
-                            helper: data => TerritoryHelper.retrieve(data),
-                            displayExpr: "territoryName",
-                            valueExpr: "id",
-                            search: "territoryName"
-                        },
-                        validationRules: ["required"]
-                    },
-                    {
-                        dataField: "photoProfiles",
-                        label: {
-                            text: "Foto"
-                        },
-                        editorOptions: {
-                            maxFile: 1,
-                            accept: ".jpg, .jpeg, .png",
-                            maxSize: 2,
-                            helper: (data) => FileHelper.upload(data),
-                            beforeUpload: () => {
-                                return {
-                                    serviceName: ServiceNameUploadConst.USER
-                                }
-                            },
-                            iconUpload: (data) => <Image showFull {...data} size="lg" />,
-                            iconRemoveUpload: () => <i className="ri-delete-bin-line fs-16 mgl-2 mgr-2"></i>
-                        },
-                        editorType: "upload",
-                        validationRules: ["required"]
-                    },
-                    {
-                        dataField: "enabled",
-                        className: "switch-label",
-                        label: {
-                            visible: false
-                        },
-                        dataType: "boolean",
-                        editorType: "switch"
-                    },
-                    {
-                        itemType: "group",
-                        name: "panel-password",
-                        colCount: 2,
-                        items: [{
-                            dataField: "password",
-                            label: {
-                                icon: "key",
-                            },
-                            editorOptions: {
-                                autoComplete: "new-password",
-                                type: "password"
-                            },
-                            validationRules: ["required", "minLength.8", {
-                                message: "Password must have at least 8 characters with number, lowercase, uppercase and one special character",
-                                validation: value => /^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/.test(value)
-                            }]
-                        }, {
-                            dataField: "confirmPassword",
-                            label: {
-                                icon: "key"
-                            },
-                            editorOptions: {
-                                autoComplete: "new-password",
-                                type: "password"
-                            },
-                            validationRules: ["required", "match.password"]
-                        }, {
-                            colSpan: 2,
-                            template: () => <div style={{marginTop: -20}} className="fs-12 text-secondary d-flex align-items-center"><i className="ri-information-line mgr-5"></i> Password must have at least 8 characters with number, lowercase, uppercase and one special character</div>
-                        }, {
-                            dataField: "showPassword",
-                            editorOptions: {
-                                onChange: () => {
-                                    showPassword = !showPassword;
-                                    formRef.current?.itemOption("password").option("editorOptions.type", showPassword ? "text" : "password")
-                                    formRef.current?.itemOption("confirmPassword").option("editorOptions.type", showPassword ? "text" : "password")
-                                }
-                            },
-                            editorType: "checkbox"
-                        }]
-                    }
                 ]
             },
         }
@@ -186,18 +54,10 @@ export default function UserManagementUserForm({ title, mode, id, hide, onSucces
     mounted(() => {
         if (id) {
             setLoading(true)
-            formRef.current?.itemOption("panel-password").option("visible", false)
-            formRef.current?.itemOption("username").option("editorOptions.disabled", true)
-
-            UserHelper.detail(id, ({ status, data }) => {
+            UserManagementHelper.detail(id, ({ status, data }) => {
                 setLoading(false)
                 if (mode === "detail") formRef.current?.disabled(true)
                 if (status) {
-                    if (isArray(data.roles, 0)) {
-                        data.roles = data.roles[0].code;
-                        data.roles === RolesEnum.SALES_COORDINATOR && visibleSalesCoordinator(data.roles)
-                    }
-
                     formRef.current?.updateData(data);
                     
                 }
@@ -222,7 +82,7 @@ export default function UserManagementUserForm({ title, mode, id, hide, onSucces
                         text: "Delete",
                         onClick: ({ loading }) => {
                             loading(true)
-                            UserHelper.delete(id, ({ status }) => {
+                            UserManagementHelper.delete(id, ({ status }) => {
                                 loading(false)
                                 status && onSuccess()
                             })
@@ -230,7 +90,7 @@ export default function UserManagementUserForm({ title, mode, id, hide, onSucces
                     }]
                 }}
             >
-                <MoreHorizRoundedIcon />
+                {/* <MoreHorizRoundedIcon /> */}
             </BgsButton>}</>}
             footer={<>
                 <BgsButton variant="text" className="btn-cancel" onClick={() => hide()}>Batal</BgsButton>
