4d8ae30b282526c3be17e00fc4fc8875b87409d3
Yeni Nur Azizah <ikanasheen.co@gmail.com>
2023-03-19 22:13:26 +0700

    yang commit sebelumnya maksudnya role admin

---
+++ src/consts/statusTransaksi.const.tsx
@@ -0,0 +1,19 @@
+import { DisplayValueStandardModel } from "models";
+
+export const StatusTransaksiConst: DisplayValueStandardModel[] = [{
+    display: "Diajukan",
+    value: "DIAJUKAN"
+}, {
+    display: "Diproses",
+    value: "DIPROSES"
+}, {
+    display: "Dibatalkan",
+    value: "DIBATALKAN"
+}, {
+    display: "Nego",
+    value: "NEGO"
+}, {
+    display: "Selesai",
+    value: "SELESAI"
+}]
+export default StatusTransaksiConst;
