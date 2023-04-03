edd1ff9359892c5fad030c08ffa24b589d10858a
Yeni Nur Azizah <ikanasheen.co@gmail.com>
2023-04-04 03:59:34 +0700

    ui profile

--- src/components/account/profile/index.tsx
+++ src/components/account/profile/index.tsx
@@ -1,16 +1,53 @@
-import { BgsButton, BgsForm, BgsTypography, FormModel } from "@andrydharmawan/bgs-component";
+import { BgsButton, BgsForm, BgsTypography, FormModel, FormRef } from "@andrydharmawan/bgs-component";
 import Box from "@mui/material/Box";
-import { credential } from "lib";
+import { credential} from "lib";
 import Image from "components/file/components/image";
 import Avatar from "@mui/material/Avatar";
+import Grid from "@mui/material/Grid";
+import KecamatanHelper from "helper/register/KecamatanHelper";
+import { useRef, useState } from "react";
+import UserHelper from "helper/UserHelper";
+
 
 export default function AccountProfileComponent() {
+    const roleId = credential.storage.get("user")?.idRole;
+    // const userId = credential.storage.get("user")?.idUser;
+    const [loading, setLoading] = useState<boolean>(false);
+    const formRef = useRef<FormRef>(null);
+    // mounted(() => {
+    //     if (id) {
+    //         setLoading(true)
+    //         UserHelper.detail(id, ({ status, data }) => {
+    //             setLoading(false)
+    //             if (status) {
+    //                 if(roleId == "3" ){
+    //                     formRef.current?.itemOption("kelurahanDesa").option("visible", true);
+    //                     formRef.current?.itemOption("kecamatan").option("visible", true);
+    //                 }
+    //                 formRef.current?.updateData(data);
+    //             }
+    //         })
+    //     }
+    // })
+
     const form: FormModel = {
         showLabelShrink: true,
         apperance: "filled",
         spacing: 2,
         formData: credential.storage.get("user"),
         colCount: 12,
+        onSubmit: (values, { reset }) => {
+
+            if (roleId == "3") {
+                formRef.current?.itemOption("kelurahanDesa").option("visible", true);
+                formRef.current?.itemOption("kecamatan").option("visible", true);
+            }
+            setLoading(true)
+            UserHelper.changeProfile(values, ({ status }) => {
+                setLoading(false)
+                if (status) reset()
+            })
+        },
         items: [
             {
                 colSpan: 3,
@@ -27,20 +64,80 @@ export default function AccountProfileComponent() {
                 itemType: "group",
                 colSpan: 9,
                 items: [
-                    `fullname|label.text=Nama Lengkap|label.icon=user|editorOptions.readOnly=true`,
-                    `email|label.icon=mail|editorOptions.readOnly=true`,
+                    `idUser|label.text=ID User|editorOptions.disabled=true`,
+                    `username|colSpan=12|label.text=Username`,
                 ],
             },
-            `phoneNumber|colSpan=12|label.text=Nomor Telepon|label.icon=phone|editorOptions.readOnly=true`,
-            `salesCategory.salesCategoryName|colSpan=12|label.text=Kategori Sales|label.icon=tree|editorOptions.readOnly=true`,
-            `territory.territoryName|colSpan=12|label.text=Territory|label.icon=tree|editorOptions.readOnly=true`,
+            `nama|label.text=Nama Lengkap|colSpan=12`,
+            `noTelepon|label.text=No Telepon|colSpan=6`,
+            `email|label.text=Email|colSpan=6`,
+            roleId == "3" ? {
+                colSpan: 6,
+                dataField: "kecamatan",
+                label: {
+                    text: "Kecamatan"
+                },
+                editorType: "select",
+                editorOptions: {
+                    helper: data => KecamatanHelper.retrieveDistrict(data),
+                    displayExpr: "name",
+                    valueExpr: "name",
+                    afterChange: {
+                        clearItems: ["kelurahanDesa"],
+                        reloadItems: ["kelurahanDesa"]
+                    },
+
+                    onChange: ({ formRef, data }) => {
+                        formRef.updateData({
+                            kecamatanId: data?.id || ""
+                        })
+                    }
+
+                },
+            } : null,
+            roleId == "3" ? {
+                colSpan: 6,
+                dataField: "kelurahanDesa",
+                label: {
+                    text: "Kelurahan/Desa"
+                },
+                editorType: "select",
+                editorOptions: {
+                    helper: data => KecamatanHelper.retrieveVillages(data),
+                    displayExpr: "name",
+                    valueExpr: "name",
+                    parameterFromField: [{
+                        opt: "filter",
+                        fromField: "kecamatanId",
+                        propReq: "id"
+                    }]
+
+                },
+            }: null,
+            `alamat|label.text=Alamat Lengkap|editorType=textarea|colSpan=12`,
+            // {
+            //     colSpan: 12,
+            //     dataField: "status",
+            //     editorType: "select",
+            //     label: {
+            //         text: "Status"
+            //     },
+            //     editorOptions: {
+            //         dataSource: StatusConst,
+            //         displayExpr: "display",
+            //         valueExpr: "value",
+            //         disabled: true
+            //     },
+            // },
         ]
     }
 
     return <>
         <BgsTypography className="title-account">Profil Saya</BgsTypography>
-        <BgsForm {...form} />
-        <BgsButton className="text-end float-end mt-3">Update</BgsButton>
+        <Grid sx={{ verflowY: "scroll", overflowX: "hidden" }}>
+            <BgsForm {...form} />
+        </Grid>
+        <BgsButton loading={loading} type="submit" visibleLoading={false} className="text-end float-end mt-3">Update</BgsButton>
 
     </>
 }
