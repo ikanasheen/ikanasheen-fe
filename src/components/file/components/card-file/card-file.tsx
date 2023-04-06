fe5a474c19c26d91fd4579953f3b543aa4e31aa5
Yeni Nur Azizah <ikanasheen.co@gmail.com>
2023-04-07 05:12:59 +0700

    revisi kecyil

--- src/components/file/components/card-file/card-file.tsx
+++ src/components/file/components/card-file/card-file.tsx
@@ -17,7 +17,7 @@ interface FileList {
 export default function CardFile({ attachment = [] }: CardFileProps) {
     return <span>
         {attachment.map((item, index) => item ? <div tabIndex={0} key={index} className="action-hover d-flex align-items-center border br-10 p-1 ps-2 pe-2" onClick={() => showFile(item)}>
-            <AttachFileIcon className="fs-14 me-1" color="success" sx={{ transform: "rotate(50deg)" }} /> <span className="text-truncate">{item.originalName}</span>
+            <AttachFileIcon className="fs-14 me-1" color="success" sx={{ transform: "rotate(50deg)" }} /> <span className="text-truncate">{item.fileName}</span>
         </div> : null)}
     </span>
 }
