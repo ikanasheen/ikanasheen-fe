e4548740fc29d40d982c60f6f5b3ff3c48afba7c
Yeni Nur Azizah <ikanasheen.co@gmail.com>
2023-05-25 12:26:00 +0700

    add default sort pengaduan

--- src/components/faq/all/list-pengaduan/index.tsx
+++ src/components/faq/all/list-pengaduan/index.tsx
@@ -35,6 +35,10 @@ export default function PengaduanList(props: MainLayoutProps) {
             propReq: "status",
             value: ['BELUM_TERJAWAB'],
             opt: "filter"
+        },{
+            propReq: "tanggalPengaduan",
+            value: "desc",
+            opt: "sort"  
         }],
         onRowClick: ({ rowData }) => form(rowData.idPengaduan),
         columns: [
